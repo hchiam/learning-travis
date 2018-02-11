c9f1aac9d9023fd3111e70b521a3a66e98168316
hchiam <hchiam@gmail.com>
2018-02-10 20:17:02 -0500

    minimal test

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-function add(num1, num2) {
-	return num1 + num2;
-}
-
-assert( add( 5, 20 ) == 25, 'Checking the add function: 5+20=25');
-assert( add( 1, 1 ) == 2, 'Checking the add function: 1+1=2');
-assert( add( 5, 20 ) == 24, 'Checking the add function: 5+20=24');
-
-function assert( passed, description ) {
-	let status = passed ? 'PASS' : 'FAIL';
-  let message = status + ': ' + description;
-  if (!passed) {
-  	throw message || "Assertion failed";
-  }
-  console.log(message);
-};
+assert(1 === 1, 'Simple test');
